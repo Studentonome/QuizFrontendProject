28b6643c3f3e9e3af1fb443e39b4e9ff3ef23aaa
Studentonome <toxicwastepoint@gmail.com>
2024-01-17 17:06:21 +0300

    changeState using

--- quizzes/proto/script.js
+++ quizzes/proto/script.js
@@ -1,4 +1,4 @@
-const maxQuestions = 5;//количество вопросов которые будут использованы в квизе
+
 const questions = [
 {//1
     mainText:'Which tool is best for digging stone and bricks?',
@@ -191,7 +191,7 @@ const questions = [
     }
 },
 ];
-
+const maxQuestions = 5;//количество вопросов которые будут использованы в квизе
 const endButtonTextToBeginQuiz = "Перейти к началу квиза";//Текст для кнопки для начала нового квиза
 const beginMainText = "Начало квиза";//Основной текст начала квиза
 const beginButtonText = "Начать квиз";//Текст на кнопке начала квиза
@@ -206,7 +206,6 @@ let opt4Text = document.getElementById("opt4Text");
 let beginButton = document.getElementById("beginButton");
 let endButtonToBegin = document.getElementById("endButtonToBegin");
 let nextQuestion = document.getElementById("nextQuestion");
-//nextQ
 
 let viewedQuestions = new Set();
 
@@ -215,26 +214,12 @@ let currentAnswer = 0;//1 - 4
 let currentQuestion = 0;
 
 let isBegin = false;
-let isEnd = false;
 let isQuestion = false;
 let isShowAns = false;
+let isEnd = true;
+let isNewQuestion = false
 
-//////////////////////////////////////////////////////////////////functions
-function allZero(){
-    isBegin = false;
-    isEnd = false;
-    isQuestion = false;
-    isShowAns = false;
-    mainText.innerHTML = "";
-    beginButton.innerHTML = "";
-    endButtonToBegin.innerHTML = "";
-    nextQuestion.innerHTML = "";
-    opt1Text.innerHTML = "";
-    opt2Text.innerHTML = "";
-    opt3Text.innerHTML = "";
-    opt4Text.innerHTML = "";
-}
-
+////////////////////////////////////////////////////////functions
 function rand(){
     console.log("rand");
     let show = (Math.floor(Math.random() * (questions.length)));
@@ -248,118 +233,128 @@ function rand(){
     
 }
 
-function addOptions(){
-    opt1Text.addEventListener("click", opt1);
-    opt2Text.addEventListener("click", opt2);
-    opt3Text.addEventListener("click", opt3);
-    opt4Text.addEventListener("click", opt4);
-}
-
-function deleteOptions(){
-    opt1Text.removeEventListener("click", opt1);
-    opt2Text.removeEventListener("click", opt2);
-    opt3Text.removeEventListener("click", opt3);
-    opt4Text.removeEventListener("click", opt4);
-}
-
-function option(){
+function allZero(){
+    isBegin = false;
+    isEnd = false;
     isQuestion = false;
-    isShowAns = true;
-    if(questions[currentQuestion]["option"+currentAnswer.toString()].correct === true)
-    {correctAnswers++;}
-    if(viewedQuestions.size === maxQuestions)
-    {nextQuestion.innerHTML = endQuizText;}
-    else
-    {nextQuestion.innerHTML = nextQuestionText;}
-    showRightAns();
-    deleteOptions();
+    isShowAns = false;
+    isNewQuestion = false
+    mainText.innerHTML = "";
+    beginButton.innerHTML = "";
+    endButtonToBegin.innerHTML = "";
+    nextQuestion.innerHTML = "";
+    opt1Text.innerHTML = "";
+    opt2Text.innerHTML = "";
+    opt3Text.innerHTML = "";
+    opt4Text.innerHTML = "";
 }
 
-function opt1() {console.log("opt1"); currentAnswer = 1; option(); }
-function opt2() {console.log("opt2"); currentAnswer = 2; option(); }
-function opt3() {console.log("opt3"); currentAnswer = 3; option(); }
-function opt4() {console.log("opt4"); currentAnswer = 4; option(); }
+function changeState(){
+    if(isBegin || isNewQuestion)
+    {
+        allZero();
+        isQuestion = true;
+        currentQuestion = rand();
+        mainText.innerHTML = questions[currentQuestion].mainText;
+        
+        viewedQuestions.add(currentQuestion);
+        
+        opt1Text.innerHTML = questions[currentQuestion].option1.text
+        opt2Text.innerHTML = questions[currentQuestion].option2.text
+        opt3Text.innerHTML = questions[currentQuestion].option3.text
+        opt4Text.innerHTML = questions[currentQuestion].option4.text
+        
+        opt1Text.addEventListener("click", opt1);
+        opt2Text.addEventListener("click", opt2);
+        opt3Text.addEventListener("click", opt3);
+        opt4Text.addEventListener("click", opt4);
+    }
+    else if(isQuestion)
+    {
+        isQuestion = false;
+        isShowAns = true;
+        if(questions[currentQuestion]["option"+currentAnswer.toString()].correct === true)
+        {correctAnswers++;}
+        if(viewedQuestions.size === maxQuestions)
+        {nextQuestion.innerHTML = endQuizText;}
+        else
+        {nextQuestion.innerHTML = nextQuestionText;}
 
-function begin(){
-    allZero();
-    isBegin = true;
+        {
+            if(currentAnswer === 1 && !questions[currentQuestion].option1.correct)
+            {opt1Text.style.backgroundColor = "firebrick";}
+            if(currentAnswer === 2 && !questions[currentQuestion].option2.correct)
+            {opt2Text.style.backgroundColor = "firebrick";}
+            if(currentAnswer === 3 && !questions[currentQuestion].option3.correct)
+            {opt3Text.style.backgroundColor = "firebrick";}
+            if(currentAnswer === 4 && !questions[currentQuestion].option4.correct)
+            {opt4Text.style.backgroundColor = "firebrick";}
 
-    viewedQuestions.clear();
-    
-    mainText.innerHTML = beginMainText;
-    beginButton.innerHTML = beginButtonText;
-};
+            if(questions[currentQuestion].option1.correct)
+            {opt1Text.style.backgroundColor = "lime";}
+            if(questions[currentQuestion].option2.correct)
+            {opt2Text.style.backgroundColor = "lime";}
+            if(questions[currentQuestion].option3.correct)
+            {opt3Text.style.backgroundColor = "lime";}
+            if(questions[currentQuestion].option4.correct)
+            {opt4Text.style.backgroundColor = "lime";}
+        }
+        {
+            opt1Text.removeEventListener("click", opt1);
+            opt2Text.removeEventListener("click", opt2);
+            opt3Text.removeEventListener("click", opt3);
+            opt4Text.removeEventListener("click", opt4);
+        }
+    }
+    else if(isShowAns)
+    {
+        {
+            opt1Text.style.backgroundColor = "steelblue";
+            opt2Text.style.backgroundColor = "steelblue";
+            opt3Text.style.backgroundColor = "steelblue";
+            opt4Text.style.backgroundColor = "steelblue";
+        }
+        if(viewedQuestions.size === maxQuestions)
+        {
+            allZero();
+            isEnd = true;
+            mainText.innerHTML = `У вас правильных ответов ${correctAnswers} из ${maxQuestions}`;
+            endButtonToBegin.innerHTML = endButtonTextToBeginQuiz;
+        }
+        else
+        {
+            allZero();
+            isNewQuestion = true;
+            changeState();
+        }
+    }
+    else if(isEnd)
+    {
+        allZero();
+        isBegin = true;
 
-function showQuestion(){
-    allZero();
-    isQuestion = true;
-
-    num = rand();
-    
-    mainText.innerHTML = questions[num].mainText;
-
-    currentQuestion = num;
-    viewedQuestions.add(currentQuestion);
-    
-    opt1Text.innerHTML = questions[num].option1.text
-    opt2Text.innerHTML = questions[num].option2.text
-    opt3Text.innerHTML = questions[num].option3.text
-    opt4Text.innerHTML = questions[num].option4.text
-    addOptions();
-};
-
-function showRightAns(){
-    if(currentAnswer === 1 && !questions[currentQuestion].option1.correct)
-    {opt1Text.style.backgroundColor = "firebrick";}
-    if(currentAnswer === 2 && !questions[currentQuestion].option2.correct)
-    {opt2Text.style.backgroundColor = "firebrick";}
-    if(currentAnswer === 3 && !questions[currentQuestion].option3.correct)
-    {opt3Text.style.backgroundColor = "firebrick";}
-    if(currentAnswer === 4 && !questions[currentQuestion].option4.correct)
-    {opt4Text.style.backgroundColor = "firebrick";}
-
-    if(questions[currentQuestion].option1.correct)
-    {opt1Text.style.backgroundColor = "lime";}
-    if(questions[currentQuestion].option2.correct)
-    {opt2Text.style.backgroundColor = "lime";}
-    if(questions[currentQuestion].option3.correct)
-    {opt3Text.style.backgroundColor = "lime";}
-    if(questions[currentQuestion].option4.correct)
-    {opt4Text.style.backgroundColor = "lime";}
+        viewedQuestions.clear();
+        
+        mainText.innerHTML = beginMainText;
+        beginButton.innerHTML = beginButtonText;
+    }
 }
-//document["opt"+currentAnswer.toString()+"Text"].style.backgroundColor = "red";
-function unshowRightAns(){
-    opt1Text.style.backgroundColor = "steelblue";
-    opt2Text.style.backgroundColor = "steelblue";
-    opt3Text.style.backgroundColor = "steelblue";
-    opt4Text.style.backgroundColor = "steelblue";
-}
 
-function end(){
-    allZero();
-    isEnd = true;
-    mainText.innerHTML = `У вас правильных ответов ${correctAnswers} из ${maxQuestions}`;
-    endButtonToBegin.innerHTML = endButtonTextToBeginQuiz;
-}
+function opt1() {console.log("opt1"); currentAnswer = 1; changeState(); }
+function opt2() {console.log("opt2"); currentAnswer = 2; changeState(); }
+function opt3() {console.log("opt3"); currentAnswer = 3; changeState(); }
+function opt4() {console.log("opt4"); currentAnswer = 4; changeState(); }
 
-//////////////////////////////////////////////////////////eventsListeners
+////////////////////////////////eventsListeners
 
 beginButton.addEventListener("mouseup",(e)=>{
-    showQuestion();
-    e.preventDefault();
+    changeState();
 })
 endButtonToBegin.addEventListener("click",(e)=>{
-    begin();
+    changeState();
 })
-
 nextQuestion.addEventListener("click",(e)=>{
-    unshowRightAns();
-    if(viewedQuestions.size === maxQuestions)
-    {end();}
-    else
-    {showQuestion();}
+    changeState();
 })
 
-////////////start with begin
-begin();
-
+changeState();
